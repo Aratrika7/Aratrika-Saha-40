17d6c7cbc3ee1cfe2433878eb292e771953bf6d8
Aratrika7 <aratrikato2020@gmail.com>
2025-01-27 19:45:17 +0530

    Add files via upload

---
+++ DAY 4 Assignment 19.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter tge value of the numbers");
+	scanf("%d %d",&a,&b);
+	if(a>b)
+	{
+		printf("the greater number is :%d",a);
+	
+	}
+	else
+	{
+		printf("the greater number is:%d",b);
+	}
+	return 0;
+}
