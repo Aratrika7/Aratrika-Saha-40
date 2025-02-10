3453ade442ea577fc0af1ab823dfd6e1fe1eaf27
Aratrika7 <aratrikato2020@gmail.com>
2025-02-10 19:02:04 +0530

    Add files via upload

---
+++ DAY 5 Assignment 24.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	printf("enter a lower case character:");
+	scanf("%c",&a);
+	if(a=='a')
+	printf("its a vowel");
+	if(a=='e')
+	printf("its a vowel");
+	if(a=='i')
+	printf("its a vowel");
+	if(a=='o')
+	printf("its a vowel");
+	if(a=='u')
+	printf("its a vowel");
+	else
+	printf("its a consonant");
+	return 0;
+	
+}
