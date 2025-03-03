1969e2c96d6294dc5e3482f4ff9d369c9879ef2e
Aratrika7 <aratrikato2020@gmail.com>
2025-03-03 17:06:04 +0530

    Add files via upload

---
+++ DAY 8 Assigment 36.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,num=1,rows=4;
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=rows-i;j++)
+		{
+			printf("-",i);
+		}
+		for (j=1;j<=i;j++)
+		{
+			printf("%d",num);num++;
+		}
+		printf("\n");
+	}
+	return 0;
+}
